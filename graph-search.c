4cda6b34e9a91e76e7ff89a215f87a5579cc7cb9
조은지 <jo-eunji@jo-eunjiui-MacBookPro.local>
2022-05-30 03:58:28 +0900

    commit2

--- graph-search.c
+++ graph-search.c
@@ -2,48 +2,72 @@
 #include <stdlib.h>
 #define Maxsize 10 //그래프 정점의 최대 개수
 
-typedef struct HeadNode{ //그래프 리스트
-    int v=0; //가지고 있는 정점 개수
-    struct Node *link; 
-}headtype;
-
 typedef struct Node{
     int vertex;
     struct Node *link;
 }nodetype;
 
-void init(headtype *h[]){
+typedef struct List{ //그래프 리스트
+    int v; //가지고 있는 정점 개수
+    nodetype *list[Maxsize]; 
+}listtype;
 
+
+void init(listtype *h){
+
+    h->v=0;
     for (int i=0;i<Maxsize;i++){
-        h[i]=NULL;
+        h->list[i]=NULL;
     } 
 
 }
-int insertvertex (headtype *h[], int v)
+int insertvertex (listtype *h, int v)
 {
-    if(v==Maxsize)
+    if((h->v)+1>Maxsize){ //최대 10개 초과
+        printf("그래프 최대 정점 개수 초과");
         return -1;
-    h->v++;
+    }
+    h->v++; //정점 개수 증가
 
     return 1;
 }
 
-int insertedge (headtype *h[],int link1, int link2)
-{
+int insertedge (listtype *h,int u, int v)
+{ //정점 번호 u와 기존 연결, v는 새로운 정점
+
+    nodetype *new; //새로운 노드 
+    if(u>=h->v ||v>=h->v){
+        printf("정점 번호 오류");
+    }
+    new->vertex=v;
+    new->link=h->list[u];
+    h->list[u]=new;
+
     return 1;
 }
 
-void dfs (headtype *h[])
+void dfs (listtype *g,int v)
+{   //탐색 시작 정점 : list[v]
+    nodetype *ptr;
+    int visit[Maxsize];
+    visit[v]=1;
+    
+    printf("[%d] ",v);
+    for (ptr=g->list[v]->link;ptr;ptr=ptr->link){
+        if(!visit[ptr->vertex])
+            dfs(g,ptr->vertex);
+    }
+    
+
+
+}
+
+void bfs (listtype *g)
 {
 
 }
 
-void bfs (headtype *h[])
-{
-
-}
-
-void print (headtype *h[])
+void print (listtype *g)
 {
 
 }
@@ -51,10 +75,10 @@ void print (headtype *h[])
 
 int main (void){
     
-    headtype *head[Maxsize];
+    listtype *list;
     char command;
     int vertex;
-    int u, v; //삽입할 edge 저장
+    int u, v; 
 
     printf("[--------------- [조은지] [2021076020] ---------------]");
     
@@ -74,30 +98,30 @@ int main (void){
 
 		switch(command) {
 		case 'z': case 'Z':
-			init(head);
+			init(list);
 			break;
 		case 'q': case 'Q':
 			break;
 		case 'v': case 'V':
 			printf("Vertex = ");
 			scanf("%d", &vertex);
-			insertvertex(head,vertex);
+			insertvertex(list,vertex);
 			break;
 		case 'e': case 'E':
-            printf("2 Edges = ");
+            printf("연결할 정점, 새로운 정점 입력 = ");
 			scanf("%d %d", &u,&v);
-			insertedge(head, u,v);
+			insertedge(list, u,v);
 			break;
 
 		case 'd': case 'D':
-			dfs(head);
+			dfs(list,v);
 			break;
 		case 'b': case 'B':
-			bfs(head);
+			bfs(list);
 			break;
 
 		case 'p': case 'P':
-			print(head);
+			print(list);
 			break;
 
 		default:
